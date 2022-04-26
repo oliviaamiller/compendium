dec10f1fdf862f8aa5b0e33b87a6fdd5eb415fc2
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-04-26 12:18:59 -0700

    added art detail and list, imported to app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import { Link, Route, Switch } from 'react-router-dom';
+import ArtDetail from './views/Art/Detail';
+import ArtList from './views/Art/List';
+
 export default function App() {
   return <h1>Hello World</h1>;
 }
