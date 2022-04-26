9094f8b04a6dfa011ce47f56e05f0a792bd7e990
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-04-26 14:53:24 -0700

    didnt need router in app.js, removed it and now rendering pokemon list

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
-import PokemonList from './views/Pokemon/List';
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+//import PokemonCard from '../../components/Pokemon/Card';
 
 export default function App() {
+
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const getPokemon = async () => {
+      const res = await fetch(' https://pokedex-alchemy.herokuapp.com/api/pokedex');
+      const { data } = await res.json();
+      const pokemonData = data.map((pokemon) => ({
+        id: pokemon.id,
+        name: pokemon.name,
+        img: pokemon.url_image
+      }))
+    }
+  });
+
   return (
-    <Router>
-    <Switch>
-      <Route path="/pokemon">
-        <PokemonList />
-      </Route>
-      <Route path="/">
-        <p>home</p>
-        <Link to="/pokemon">pokemon list</Link>
-      </Route>
-    </Switch>
-    </Router>
+    <p>pokemon list</p>
   );
 }
